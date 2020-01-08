79d5812ddf57e632aa648e8656302c14198080fa
Vlad Dyukov <dyukovlad@MacBook-Pro-Vlad.local>
2020-01-08 12:40:33 +0300

    edit full app

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+
+const app = express();
+console.error("Start backend build");
+app.use(express.json({ extended: true }));
+
+app.use("/api/card", require("./routes/card.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = config.get("port") || 5000;
+
+async function start() {
+  try {
+    await mongoose
+      .connect(config.get("mongoURI"), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      })
+      .then(console.log("DB Connection Successfull"))
+      .catch(err => {
+        console.error(err);
+      });
+    //run
+    app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+  } catch (error) {
+    console.log("Server error", error.message);
+    process.exit(1);
+  }
+}
+
+start();
