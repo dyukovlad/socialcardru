fd8552c07c979fc3094e5de1efd5a29adfa79b85
Vlad Dyukov <dyukovlad@MacBook-Pro-Vlad.local>
2020-01-14 20:06:54 +0300

    add new api and uri

--- routes/card.routes.js
+++ routes/card.routes.js
@@ -153,64 +153,91 @@ router.post("/payment", async (req, res) => {
   }
 });
 
-// /getPrice
+// /payment/responce
+// Order_ID —номер заказа;
+// Status — статус (authorized, paid, canceled);
+// Signature
 router.post("/payment/response", async (req, res) => {
   try {
-    const {
-      client_time,
-      card,
-      id,
-      pdcode,
-      date,
-      paydate,
-      summa,
-      payinfo,
-      abonent,
-      Signature
-    } = req.body;
+    const { Order_ID, Status, Signature } = req.body;
+    console.log(req.body);
+    if (Status === "paid") {
+      await user.findOneAndUpdate(
+        { Signature: Signature },
+        { orderIdPayment: Order_ID },
+        options,
+        callback
+      );
 
-    let xmlBodyStr = `<?xml version="1.0" encoding="windows-1251"?>
-    <message><head>
-      <api_version>1</api_version>
-      <software_version>1</software_version>
-      <message_type>Payment</message_type>
-      <device_type>LK</device_type>
-      <terminal_code>${config.get("terminal_code")}</terminal_code>
-      <system_code>${config.get("system_code")}</system_code>
-      <client_time>${client_time}</client_time>
-    </head><body>
-    <printnum>${card}</printnum>
-    <id>${id}</id>
-    <pdcode>${pdcode}</pdcode>
-    <type>1</type>
-    <date>${date}</date>
-      <paydate>${paydate}</paydate>
-      <summa>${summa}</summa>
-      <payinfo>${payinfo}</payinfo >
-      <payform>1</payform >
-      <abonent>${abonent}</abonent >
-      <operator>0</operator >  
-    </body></message>`;
-
-    let body = await axios
-      .post(config.get("url_asop"), xmlBodyStr, {
-        responseType: "arraybuffer"
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-
-    let responseData = iconv.decode(body.data, "win1251");
-
-    // console.log(str);
+      const currentCardPayment = await user.find({ Signature: Signature });
+    }
 
     //возвращаем на фронт
-    res.status(201).json({ data: responseData });
+    res.status(201).json({ data: currentCardPayment });
 
     //send checkcard
   } catch (e) {
     res.status(500).json({ message: "Что то пошло не так" });
   }
 });
+
+// router.post("/payment/response", async (req, res) => {
+//   try {
+//     const {
+//       client_time,
+//       card,
+//       id,
+//       pdcode,
+//       date,
+//       paydate,
+//       summa,
+//       payinfo,
+//       abonent,
+//       Signature
+//     } = req.body;
+
+//     let xmlBodyStr = `<?xml version="1.0" encoding="windows-1251"?>
+//     <message><head>
+//       <api_version>1</api_version>
+//       <software_version>1</software_version>
+//       <message_type>Payment</message_type>
+//       <device_type>LK</device_type>
+//       <terminal_code>${config.get("terminal_code")}</terminal_code>
+//       <system_code>${config.get("system_code")}</system_code>
+//       <client_time>${client_time}</client_time>
+//     </head><body>
+//     <printnum>${card}</printnum>
+//     <id>${id}</id>
+//     <pdcode>${pdcode}</pdcode>
+//     <type>1</type>
+//     <date>${date}</date>
+//       <paydate>${paydate}</paydate>
+//       <summa>${summa}</summa>
+//       <payinfo>${payinfo}</payinfo >
+//       <payform>1</payform >
+//       <abonent>${abonent}</abonent >
+//       <operator>0</operator >
+//     </body></message>`;
+
+//     let body = await axios
+//       .post(config.get("url_asop"), xmlBodyStr, {
+//         responseType: "arraybuffer"
+//       })
+//       .catch(err => {
+//         res.status(500).json(err);
+//       });
+
+//     let responseData = iconv.decode(body.data, "win1251");
+
+//     // console.log(str);
+
+//     //возвращаем на фронт
+//     res.status(201).json({ data: responseData });
+
+//     //send checkcard
+//   } catch (e) {
+//     res.status(500).json({ message: "Что то пошло не так" });
+//   }
+// });
 
 module.exports = router;
